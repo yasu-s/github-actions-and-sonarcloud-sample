bfcd2b381de3f034aa31b40ef1340cda3a2920fc
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2022-01-17 15:40:21 +0900

    テストケース追加

---
+++ test/deep-copy-test.ts
@@ -0,0 +1,15 @@
+import '../src/ext/object/deep-copy';
+
+describe('Object.deepCopy', () => {
+  it.each([
+    { case: 'object', obj: { id: 1, name: 'hoge' }, expected: { id: 1, name: 'hoge' } },
+    { case: 'nested object', obj: { data: [{ id: 1, name: 'hoge' }] }, expected: { data: [{ id: 1, name: 'hoge' }] } },
+  ])('$case', ({ obj, expected }) => {
+    // exercise
+    const actual = obj.deepCopy<{ id: number; name: string }>();
+
+    // verify
+    expect(actual).not.toBe(obj);
+    expect(actual).toEqual(expected);
+  });
+});
