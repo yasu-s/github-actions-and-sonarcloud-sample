4a97f5453b4197ec243aee73ea32fbf937923177
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2022-01-17 15:23:47 +0900

    テストケース追加

---
+++ test/equal-to-test.ts
@@ -0,0 +1,35 @@
+import '../src/ext/object/equal-to';
+
+describe('Object.equalTo', () => {
+  describe('object', () => {
+    it.each([
+      { case: 'ok', target: { id: 1, name: 'hoge' }, expected: true },
+      { case: 'ng', target: { id: 2, name: 'hoge' }, expected: false },
+    ])('$case', ({ target, expected }) => {
+      // setup
+      const obj = { id: 1, name: 'hoge' };
+
+      // exercise
+      const actual = obj.equalTo(target);
+
+      // verify
+      expect(actual).toBe(expected);
+    });
+  });
+
+  describe('nested object', () => {
+    it.each([
+      { case: 'ok', target: { data: [{ id: 1, name: 'hoge' }] }, expected: true },
+      { case: 'ng', target: { data: [{ id: 2, name: 'hoge' }] }, expected: false },
+    ])('$case', ({ target, expected }) => {
+      // setup
+      const obj = { data: [{ id: 1, name: 'hoge' }] };
+
+      // exercise
+      const actual = obj.equalTo(target);
+
+      // verify
+      expect(actual).toBe(expected);
+    });
+  });
+});
