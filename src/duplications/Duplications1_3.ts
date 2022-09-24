7255f0d64085a0d8f7f3dfcaf009a36c6bc3f125
Yasuyuki Saito <ya_saito@uluru.jp>
2022-09-24 13:02:06 +0900

    test

--- src/duplications/Duplications1_3.ts
+++ src/duplications/Duplications1_3.ts
@@ -1,79 +1,16 @@
 export class Hoge2 {
-  log() {
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
-    console.log('aaa');
-    console.log('bbb');
-    console.log('ccc');
-    console.log('ddd');
-    console.log('eee');
+  log2() {
+    () => {
+      console.log('aaa');
+      () => {
+        console.log('aaa');
+        () => {
+          console.log('aaa');
+          () => {
+            console.log('aaa');
+          }
+        }
+      }
+    }
   }
 }
